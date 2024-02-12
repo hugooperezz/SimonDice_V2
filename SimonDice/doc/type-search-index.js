8a4f665a142442108529eb44aab2017056e6ebfd
hugooperezz <hugo.perez.redondo@colegiolitterator.com>
2024-02-12 17:03:53 +0100

    Java doc

---
+++ SimonDice/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"SimonDice","l":"engine"},{"p":"SimonDice","l":"Main"},{"p":"SimonDice","l":"persona"},{"p":"SimonDice","l":"engine.tColores"}];updateSearchResults();
